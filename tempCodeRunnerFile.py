ac9a0336f630e5a749748076de10fe2fb82fdade
hksoriginal <44989568+hksoriginal@users.noreply.github.com>
2021-10-02 10:52:01 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-target = r'C:\Users\hksor\Downloads\Telegram Desktop\Volume 1'
-# ext = '.txt'
-# for x in os.listdir(target):
-#     if x.endswith(ext):
-#         print(f'Deleting File {x}')
-#         os.unlink(target + '\\' + x)
-#         time.sleep(0.5)
-#         print('File Deleted')
